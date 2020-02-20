c7f62c43531f266bdd9e7616cb9e53ffab2c08be
Brady Trainor <mail@bradyt.net>
2020-02-20 00:02:16 -0800

    Add a widget test with a stateful widget

--- test/flutter_typeahead_test.dart
+++ test/flutter_typeahead_test.dart
@@ -1,1 +1,47 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+void main() {
+  testWidgets('Test.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: DescriptionField(),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+  });
+}
+
+class DescriptionField extends StatefulWidget {
+  const DescriptionField();
+
+  @override
+  DescriptionFieldState createState() => DescriptionFieldState();
+}
+
+class DescriptionFieldState extends State<DescriptionField> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TypeAheadField<String>(
+      textFieldConfiguration: TextFieldConfiguration<dynamic>(
+        controller: controller,
+        autofocus: true,
+      ),
+      suggestionsCallback: (_) => [],
+      itemBuilder: (_, __) => Container(),
+      onSuggestionSelected: (_) {},
+    );
+  }
+}
